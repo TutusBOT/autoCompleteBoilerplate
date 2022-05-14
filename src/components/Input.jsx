68391ceca4df956f3d990a2bd6490ef0d9194079
TutusBOT <barte77pl@gmail.com>
2022-05-15 00:23:27 +0200

    chore: some refactors

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import store from "../redux/store";
 import { getUsers, initialState } from "../redux/users";
 
 function Input() {
-	const [users, setUsers] = useState(initialState);
+	// const [users, setUsers] = useState(initialState);
 	const [inputValue, setInputValue] = useState("");
 	const [autoComplete, setAutoComplete] = useState([]);
+	const users = useSelector((state) => state.users);
 
 	useEffect(() => {
 		store.dispatch(getUsers());
-		store.subscribe(() => {
-			setUsers(store.getState().users);
-		});
 	}, []);
 
 	useEffect(() => {
-		if (users !== initialState) {
-			if (!inputValue) return setAutoComplete([]);
-			setAutoComplete(
-				users.data.filter(({ name }) => {
-					if (name === inputValue) return;
-					return name.toLowerCase().includes(inputValue.toLowerCase());
-				})
-			);
-		}
+		if (users.error) console.log(users.error);
+		if (!users.data || !inputValue) return setAutoComplete([]);
+		setAutoComplete(
+			users.data.filter(({ name }) => {
+				return (
+					name.toLowerCase().includes(inputValue.toLowerCase()) &&
+					name !== inputValue
+				);
+			})
+		);
 	}, [inputValue]);
 
 	return (
